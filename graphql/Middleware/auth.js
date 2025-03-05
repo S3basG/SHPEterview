aaf92d004615230b3ca1965d89bf33e511741228
sgarcia2022 <sebthegeniusgarcia@gmail.com>
2025-03-05 14:25:50 -0500

    Fixed file location

--- graphql/Middleware/auth.js
+++ graphql/Middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../config'); // Ensure SECRET_KEY is exported from your config
+const { SECRET_KEY } = require('../../config'); // Ensure SECRET_KEY is exported from your config
 
 
 module.exports = (context) => {
